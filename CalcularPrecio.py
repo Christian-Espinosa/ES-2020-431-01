ee469e0bb8ccb2f2743ad3c2e334f4de87e8a092
Anna Dot <annadotmariano@gmail.com>
2020-05-14 10:34:17 +0200

    no message

---
+++ CalcularPrecio.py
@@ -0,0 +1,15 @@
+class CalcularPrecio:
+     def __init__(self,,):
+        self.metodoPago = player1
+        self.NumPasajeros = player2
+        self.DiasEstanciaH = player2
+        self.DiasCoche=
+        importe hotel
+        importe coche
+        importe vuelo
+        numero dias vuelo
+
+
+        def __init__(self, name:str):
+        self.name = name
+        self.num_points = 0
