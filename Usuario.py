fba6772e0c719cea03fb17ff727f2303a5d12e98
Anna Dot <annadotmariano@gmail.com>
2020-05-15 17:44:52 +0200

    Creación de la API usuario

---
+++ Usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+   def __init__(self, NombreCompleto:str, DNI:str, DirPostal:str, NumTel: str, Email: str):
+      self.NombreCompleto = NombreCompleto
+      self.DNI = DNI
+      self.DirPostal = DirPostal
+      self.NumTel= NumTel
+      self.Email= Email
