75d5a85cf66aa6f904eb47fbc2f3f2b23b5bc91e
Anna Dot <annadotmariano@gmail.com>
2020-05-15 18:03:19 +0200

    Eliminació de la classe "Hotel.py" la bona és Hotels.py

--- Hotel.py
+++
@@ -1,8 +0,0 @@
-class Hotel:
-    def __init__(self, dias:int, ID:str, numHab:int):
-        self.dias=dias
-        self.ID=ID
-        self.precio=0
-        self.numHab=numHab
-
-    #conectar amb API
