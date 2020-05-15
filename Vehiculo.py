8e6401c76b3fc51ea9e6c8b3a2eb056b01bc1707
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2020-05-15 17:44:27 +0200

    Creacion clases Vehiculo y Vuelo fase inicial

---
+++ Vehiculo.py
@@ -0,0 +1,19 @@
+class Vehiculo:
+    
+    id=""
+    precio=0
+    dias=0
+
+    def __init__(self,id:str, precio, dias):
+        self.id=id
+        self.precio=precio
+        self.dias=dias
+
+    def get_id(self)->str:
+        return self.id
+
+	def get_precio(self):
+        return self.precio
+        
+    def get_dias(self):
+        return self.dias
