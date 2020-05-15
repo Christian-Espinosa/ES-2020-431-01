d3593c0230e2c0256d25af3eb2742d6db18b5831
Anna Dot <annadotmariano@gmail.com>
2020-05-15 17:43:00 +0200

    Creación de la clase Hotel

---
+++ Hotel.py
@@ -0,0 +1,8 @@
+class Hotel:
+    def __init__(self, dias:int, ID:str, numHab:int):
+        self.dias=dias
+        self.ID=ID
+        self.precio=0
+        self.numHab=numHab
+
+    #conectar amb API
