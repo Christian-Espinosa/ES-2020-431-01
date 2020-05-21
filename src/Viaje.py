f2184b701997e2a68691e9d3828a1c6a5941660f
Anna Dot <annadotmariano@gmail.com>
2020-05-21 11:30:27 +0200

    Correción errores rm coche

--- src/Viaje.py
+++ src/Viaje.py
@@ -60,7 +60,7 @@ class Viaje:
                 if d.get_id()==id_dest:
                     d.hotel=None
     
-    def remove_car(self, id_car):
+    def remove_car(self, id_dest):
         if len(self.Destinos_Obj.get_lista_destinos()) > 0:
             for d in self.Destinos_Obj.get_lista_destinos():
                 if d.get_id()==id_dest
