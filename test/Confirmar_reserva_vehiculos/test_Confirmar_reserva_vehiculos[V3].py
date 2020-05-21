f36dad392cef1650694bc022946289af4d5fc7f7
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2020-05-21 17:45:48 +0200

    Limpieza codigo Test Calcular Precio Total V1

--- test/Confirmar_reserva_vehiculos/test_Confirmar_reserva_vehiculos[V3].py
+++ test/Confirmar_reserva_vehiculos/test_Confirmar_reserva_vehiculos[V3].py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 05:16:41 2020
+
+@author: usuari
+"""
 import os
 import sys
 import unittest
-
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 PROJECT_DIR = os.path.join(PROJECT_DIR, '..', 'src')
@@ -9,36 +14,18 @@ if not PROJECT_DIR is sys.path:
     sys.path.insert(0, PROJECT_DIR)
 
 from Confirmar_reserva import Confirmar_Reserva
+from requests.exceptions import Timeout
+from unittest import mock
 
+from API.Coche import Rentalcars as Rentalcars
+from API.Coche import Cars as Cars
 
+class TestMock1(unittest.TestCase):
+    @mock.patch('Confirmar_reserva.os.path')
 
-"""
-Dado un viaje con múltiples destinos y más de un viajero, cuando se confirma
-correctamente la reserva de los vuelos, se reporta que la acción se ha realizado
-correctamente
+    rc=Rentalcars.Rentalcars()
+    return rc.confirm_reserve("787372-P", "7351VEL")
 
-"""
+    def test_Confirmar_reserva(self, mock_Reserva):
 
-
-class TestConfirmarReservaVehiculos(unittest.TestCase):
-
-    #def setUpClass(cls):
-        #cls.
-
-
-        """
-        player1 = Player("player1")
-        player2 = Player("player2")
-        game = Game(player1, player2)
-
-        score_max = max(score_player_1, score_player_2)
-        for score in range(0, score_max):
-            if score < score_player_1:
-                game.won_point(player1)
-            if score < score_player_2:
-                game.won_point(player2)
-
-        score = game.get_score()
-
-        assert score == expected_result
-        """
+        self.assertTrue(mock_Reserva.ConfirmacioReserva("Iberia","US-1"))
