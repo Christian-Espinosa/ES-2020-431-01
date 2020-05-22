3fed182d284c7790c5c0b175525a1417058c2613
Christian <chr.espinosa@gmail.com>
2020-05-22 19:24:10 +0200

    [V4] Primera versión Gestion metodo de pago para contar cuantas veces se intenta pagar

--- src/GestionarMetodoPago.py
+++ src/GestionarMetodoPago.py
@@ -8,21 +8,36 @@ class GestionarMetodoPago:
     
     def __init__(self, precio, Viaje_Obj, metodo):
         self.done = False
-        self.metodo = None
-        if 1<len(Viaje_Obj.Viatgers_Obj.get_viatgers()) and 1<Viaje_Obj.Destinos_Obj.get_num_destinos():
+        self.metodo = metodo
+        self.pagar_count = 0
+        self.viaje = Viaje_Obj
+        gestor_de_caracteristicas(self)
+
+    def gestor_de_caracteristicas(self)
+        if 1<len(self.viaje.Viatgers_Obj.get_viatgers()) and 1<self.viaje.Destinos_Obj.get_num_destinos():
             if metodo == "MasterCard":
                 self.done = True
-                self.metodo = "MasterCard"
             elif metodo == "Visa":
                 self.done = True
-                self.metodo = "Visa"
+                
             if self.done == True:
                 self.viaje = Viaje_Obj
                 self.importe = precio
                 self.usuario = Viaje_Obj.User_Obj
-        
+                
+    def set_viatje(self, Viaje_Obj):
+        self.viaje = Viatgers_Obj
+
         
     def Pagar(self):
-        if self.done:
-            return Bank().do_payment(self.usuario, self.metodo, self.importe)
+        #esta funcion pagar tiene en cuenta que usuario la ha ejecutado!
+        gestor_de_caracteristicas(self)
+        if self.pagar_count < 3:
+            if self.done:
+                return Bank().do_payment(self.usuario, self.metodo, self.importe)
+            else:
+                #error
+                self.pagar_count += 1
+        else:
+            return False:
 
